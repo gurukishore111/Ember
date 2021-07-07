82ed8d65c56195f10637f767c462f1a186d9aa9b
Guru Kishore <guru.kishore@wylo.in>
2021-07-07 11:40:45 +0530

    unit testing in ember

--- tests/unit/services/shopping-cart-test.js
+++ tests/unit/services/shopping-cart-test.js
@@ -1,12 +1,19 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Service | shopping-cart', function(hooks) {
+module('Unit | Service | shopping-cart', function (hooks) {
   setupTest(hooks);
 
   // TODO: Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('it exists', function (assert) {
     let service = this.owner.lookup('service:shopping-cart');
     assert.ok(service);
+  });
+
+  test('addItem works', function (assert) {
+    const service = this.owner.lookup('service:shopping-cart');
+    assert.equal(service.itemList.length, 0, 'Item list is empty');
+    service.addItem({});
+    assert.equal(service.itemList.length, 1, 'Item is increased by 1');
   });
 });
